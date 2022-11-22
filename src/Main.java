51a107e5af3950a85207e907fbe5a4754e9d68d9
ilyas Arikan <ilyasarikan@gmail.com>
2022-11-22 21:30:09 +0300

    comma

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("bir sayi ltfn: ");
+        int n = inp.nextInt();
+        for(int i = 1; i<=n;i++){
+            for(int k = 1; k<=i;k++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ;j <= (2*n-(2*i+1));j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
